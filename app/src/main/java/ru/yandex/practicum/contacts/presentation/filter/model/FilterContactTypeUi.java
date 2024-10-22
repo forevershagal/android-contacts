67cfbb46256dabfddd56656fc292d674b9d30c0e
forevershagal <07ramilshagaliev@gmail.com>
2024-10-22 17:03:41 +0300

    Add dev branch for Y.Practicum

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
@@ -2,7 +2,9 @@ package ru.yandex.practicum.contacts.presentation.filter.model;
 
 import androidx.annotation.NonNull;
 
-public class FilterContactTypeUi {
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+
+public class FilterContactTypeUi implements ListDiffInterface<FilterContactTypeUi> {
 
     private final FilterContactType contactType;
     private final boolean selected;
@@ -19,6 +21,13 @@ public class FilterContactTypeUi {
     public boolean isSelected() {
         return selected;
     }
+
+    @Override
+    public boolean theSameAs(FilterContactTypeUi listItem)
+    {
+        return this.getContactType() == listItem.getContactType();
+    }
+
 
     @Override
     public boolean equals(Object o) {
